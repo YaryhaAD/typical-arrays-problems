9d4e072f64f3614f5b35fc5fbeca1c44acf3dbf2
YaryhaAD <YaryhaAD@gmail.com>
2021-08-18 17:17:38 +0300

    feat: create all functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+
+  if (typeof array === 'undefined' || array.length === 0){
+    return 0;
+  }else {
+    return Math.min(...array);
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (typeof array === 'undefined' || array.length === 0){
+    return 0;
+  }else {
+    return Math.max(...array);
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  var total = 0;
+  var count = 0;
+
+  if (typeof array === 'undefined' || array.length === 0){
+    return 0;
+  }else {
+    array.forEach(function (item, i) {
+      total += item;
+      count++;
+    });
+  
+    return total / count;
+    }
 }
+
+
